c0ac43c70b1562a30907c760a8664007c8b5d22d
Mario <marioperdiguero37@gmail.com>
2024-11-04 22:14:51 +0100

    🎉 Initialize Project

---
+++ src/main/java/safa/safepaws/SafePawsBackendApplication.java
@@ -0,0 +1,13 @@
+package safa.safepaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePawsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafePawsBackendApplication.class, args);
+    }
+
+}
