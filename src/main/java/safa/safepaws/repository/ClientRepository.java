e401eea6d563b3e5d232bb1a8ceed4213919d3b7
Raúl <rcruzadodelgado@safareyes.es>
2024-11-08 17:04:49 +0100

    ✨ ADD java.util.Optional

--- src/main/java/safa/safepaws/repository/ClientRepository.java
+++ src/main/java/safa/safepaws/repository/ClientRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     @Override
-    Optional<Client> findById(Integer integer);
+    Optional<Client> findById(Integer ClientId);
 }
