ba50a5d0e06e64067e75e2eb9feba73aaf526eaa
Mario <mperdiguerobarrera@safareyes.es>
2024-11-08 18:45:29 +0100

    ✨ Create User Service

--- src/main/java/safa/safepaws/service/UserService.java
+++ src/main/java/safa/safepaws/service/UserService.java
@@ -1,10 +1,12 @@
 package safa.safepaws.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import safa.safepaws.dto.authentication.AuthenticationRequest;
 import safa.safepaws.dto.authentication.AuthenticationResponse;
 import safa.safepaws.dto.user.CreateUserRequest;
@@ -22,11 +24,32 @@ public class UserService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
+    private boolean checkIfUserExists(String username) {
+        return userRepository.findTopByUsername(username).isPresent();
+    }
+
     public AuthenticationResponse register(CreateUserRequest createUserRequest) {
+        if (createUserRequest.getUsername().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username is required");
+        }
+
+        if (createUserRequest.getEmail().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email is required");
+        }
+
+        if (createUserRequest.getPassword().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password is required");
+        }
+
+        if (checkIfUserExists(createUserRequest.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists");
+        }
+
         var user = new User().builder()
                 .username(createUserRequest.getUsername())
                 .password(passwordEncoder.encode(createUserRequest.getPassword()))
                 .email(createUserRequest.getEmail())
+                .client(createUserRequest.getClient())
                 .rol(Role.USER)
                 .build();
 
