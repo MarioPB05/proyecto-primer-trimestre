67aae5d3083cb20ac02ba81aa14eda63d95092cd
Miguel Rico <mricofernandez@safareyes.es>
2024-11-08 17:56:40 +0100

    ♻️ Update enum functions

--- src/main/java/safa/safepaws/enums/AnimalType.java
+++ src/main/java/safa/safepaws/enums/AnimalType.java
@@ -1,5 +1,28 @@
 package safa.safepaws.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
 public enum AnimalType {
-    DOG, CAT
+    PERRO (1),
+    GATO (2);
+    private final int id;
+
+    @JsonValue
+    public int getId(){
+        return id;
+    }
+
+    @JsonCreator
+    public static AnimalType fromId(int id){
+        for (AnimalType type : AnimalType.values()){
+            if (type.getId() == id){
+                return type;
+            }
+        }
+        return null;
+    }
 }
