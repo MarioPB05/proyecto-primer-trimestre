73ecf9bae1f960375649442b37d72569c0c3c0fe
Mario <mperdiguerobarrera@safareyes.es>
2024-11-14 17:10:21 +0100

    🐛 Set extends code

--- src/main/java/safa/safepaws/repository/QuestionRepository.java
+++ src/main/java/safa/safepaws/repository/QuestionRepository.java
@@ -1,11 +1,12 @@
 package safa.safepaws.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import safa.safepaws.model.Question;
 
 import java.util.Optional;
 
 @Repository
-public interface QuestionRepository {
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Optional<Question> findQuestionById(Integer id);
 }
--- src/main/java/safa/safepaws/repository/RequestAnswerRepository.java
+++ src/main/java/safa/safepaws/repository/RequestAnswerRepository.java
@@ -1,5 +1,6 @@
 package safa.safepaws.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import safa.safepaws.model.RequestAnswer;
 
@@ -7,6 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RequestAnswerRepository {
+public interface RequestAnswerRepository extends JpaRepository<RequestAnswer, Integer> {
     List<RequestAnswer> findAllByRequestId(Integer requestId);
 }
