2c87adf513e18fcfab13c31fc5bbb486ec4d58b9
Mario <mperdiguerobarrera@safareyes.es>
2024-11-14 16:09:17 +0100

    ✨ Create service methods to check if token is valid

--- src/main/java/safa/safepaws/service/UserService.java
+++ src/main/java/safa/safepaws/service/UserService.java
@@ -1,19 +1,29 @@
 package safa.safepaws.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import safa.safepaws.dto.authentication.AuthenticationRequest;
 import safa.safepaws.dto.authentication.AuthenticationResponse;
+import safa.safepaws.dto.authentication.VerifyAuthResponse;
 import safa.safepaws.dto.user.CreateUserRequest;
 import safa.safepaws.enums.Role;
 import safa.safepaws.model.User;
 import safa.safepaws.repository.UserRepository;
 import safa.safepaws.security.JwtService;
+
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -23,6 +33,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
 
     private boolean checkIfUserExists(String username) {
         return userRepository.findTopByUsername(username).isPresent();
@@ -72,6 +83,34 @@ public class UserService {
         var token = jwtService.generateToken(user);
 
         return AuthenticationResponse.builder().token(token).build();
+    }
+
+    private VerifyAuthResponse notValidToken() {
+        return VerifyAuthResponse.builder().valid(false).build();
+    }
+
+    private VerifyAuthResponse validToken(Date expiresAt) {
+        return VerifyAuthResponse.builder().valid(true).expiresAt(expiresAt.toString()).build();
+    }
+
+    public VerifyAuthResponse verifyToken(String token) {
+        if (token == null || !token.startsWith("Bearer ")) {
+            return notValidToken();
+        }
+
+        try {
+            token = token.substring(7);
+            String username = jwtService.extractUsername(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (userDetails == null || !jwtService.isTokenValid(token, userDetails)) {
+                return notValidToken();
+            }
+
+            return validToken(jwtService.extractClaim(token, Claims::getExpiration));
+        }catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            return notValidToken();
+        }
     }
 
 }
