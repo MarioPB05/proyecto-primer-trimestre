3e2db2c53cd51b01954e1b82fa533cf292a8f727
Mario <mperdiguerobarrera@safareyes.es>
2024-11-15 15:38:15 +0100

    🔥 Remove unnecessary files

--- src/main/java/safa/safepaws/cloudinary/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package safa.safepaws.cloudinary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ImageRepository  extends JpaRepository<Image, UUID> {
-}
