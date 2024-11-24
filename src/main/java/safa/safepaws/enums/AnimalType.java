f39cb5550c00c55f3a36702726190f1595d5f6d0
Mario <mperdiguerobarrera@safareyes.es>
2024-11-24 17:25:47 +0100

    ♻️ Add new animal types

--- src/main/java/safa/safepaws/enums/AnimalType.java
+++ src/main/java/safa/safepaws/enums/AnimalType.java
@@ -8,7 +8,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AnimalType {
     PERRO (0),
-    GATO (1);
+    GATO (1),
+    CONEJO(2),
+    HAMSTER(3),
+    TORTUGA(4),
+    PECES(5),
+    PAJARO(6),
+    COBAYA(7),
+    HURON(8),
+    POLLITO(9),
+    CABALLO(10),
+    CHINCHILLA(11),
+    RATA(12),
+    SERPIENTE(13),
+    LAGARTO(14),
+    ERIZO(15),
+    PATO(16),
+    CAMALEON(17),
+    IGUANA(18),
+    OVEJA(19);
+
     private final int id;
 
     @JsonValue
