2dba05a8f3cbd9ca993e90d133b470c14741ce03
Mario <mperdiguerobarrera@safareyes.es>
2024-11-26 23:09:18 +0100

    ♻️ Send correct response

--- src/main/java/safa/safepaws/controller/UserController.java
+++ src/main/java/safa/safepaws/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import safa.safepaws.dto.user.EditUserRequest;
 import safa.safepaws.dto.user.GetUserResponse;
-import safa.safepaws.model.User;
 import safa.safepaws.service.CloudinaryService;
 import safa.safepaws.service.UserService;
 
@@ -18,23 +17,18 @@ public class UserController {
     private final UserService userService;
     private final CloudinaryService cloudinaryService;
 
-   // @RequestBody EditUserRequest editUserRequest
     @PutMapping("/edit")
-    public ResponseEntity<String> editUser(@RequestPart("dto") EditUserRequest editUserRequest,
-                                         @RequestPart(value="file", required = false) MultipartFile file ) {
+    public ResponseEntity<GetUserResponse> editUser(@RequestPart("dto") EditUserRequest editUserRequest,
+                                                    @RequestPart(value="file", required = false) MultipartFile file ) {
         try {
             if (file != null) {
                 editUserRequest.getClient().setPhoto(cloudinaryService.uploadImage(file));
             }
 
-            User updatedUser = userService.editUser(editUserRequest);
-
-            // return ResponseEntity.ok(updatedUser);
+            return ResponseEntity.ok(userService.editUser(editUserRequest));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-
-        return  ResponseEntity.ok("PEPE");
     }
 
     @GetMapping("/me")
