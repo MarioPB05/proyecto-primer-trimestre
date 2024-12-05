8767638ec94d5daffa146a519035ef9a020705a4
Mario <mperdiguerobarrera@safareyes.es>
2024-12-05 13:36:43 +0100

    ♻️ Add order by

--- src/main/java/safa/safepaws/repository/QuestionRepository.java
+++ src/main/java/safa/safepaws/repository/QuestionRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
-    @Query("SELECT q FROM Question q WHERE q.deleted = false")
+    @Query("SELECT q FROM Question q WHERE q.deleted = false ORDER BY q.id")
     List<Question> findUndeletedQuestion();
 
     Optional<Question> findById(Integer questionId);
