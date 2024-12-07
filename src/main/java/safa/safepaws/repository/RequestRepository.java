53d6300068d82e7c0afc225300cc2fff268b12ba
Mario <mperdiguerobarrera@safareyes.es>
2024-12-07 23:45:45 +0100

    ✨ Add methods to find a request

--- src/main/java/safa/safepaws/repository/RequestRepository.java
+++ src/main/java/safa/safepaws/repository/RequestRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import safa.safepaws.enums.RequestStatus;
 import safa.safepaws.model.Request;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 @Repository
 public interface RequestRepository  extends JpaRepository<Request, Integer> {
 
-    Optional<Request> findAllById(Integer id);
+    Optional<Request> findByCode(String code);
+
+    Optional<Request> findByClientIdAndPostIdAndDeletedIsFalse(Integer clientId, Integer postId);
 
     @Query("SELECT r FROM Request r WHERE r.client.id = :clientId AND r.deleted = false")
     List<Request> findAllByClientIdSent(@Param("clientId") Integer clientId);
